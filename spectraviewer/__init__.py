419ff1bea6366872048db1c603bd1be43d54bbbb
Jakub Koza <kozajaku@fit.cvut.cz>
2017-03-14 19:59:40 +0100

    Changed implicit filesystem and jobs dir paths

--- spectraviewer/__init__.py
+++ spectraviewer/__init__.py
@@ -5,10 +5,11 @@ from tornado.options import define, options, parse_command_line
 
 define('port', default='7000')
 define('filesystem_path', default=os.environ.get('FILESYSTEM',
-                                                 '/home/radiokoza/Plocha/spectra'))  # todo change
+                                                 '/tmp/filesystem'))
 define('jobs_path', default=os.environ.get('JOBS',
-                                           '/home/radiokoza/Stažené'))  # todo change
+                                           '/tmp/jobs'))
 define('legend_hide_threshold', default='10')
+
 
 def main():
     parse_command_line()
